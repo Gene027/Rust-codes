404cc1c77ef050de2bfc7821f4d5b8e59f558e7d
Gene027 <100707312+Gene027@users.noreply.github.com>
2022-07-26 18:46:26 +0100

    Delete rustling directory

--- rustling/target/debug/deps/rustling-de259d0e0a7a8000.d
+++
@@ -1,5 +0,0 @@
-/home/praise/Documents/GitHub/rust-codes/rustling/target/debug/deps/rustling-de259d0e0a7a8000.rmeta: src/main.rs
-
-/home/praise/Documents/GitHub/rust-codes/rustling/target/debug/deps/rustling-de259d0e0a7a8000.d: src/main.rs
-
-src/main.rs:
--- rustling/target/debug/rustling.d
+++
@@ -1,1 +0,0 @@
-/home/praise/Documents/GitHub/rust-codes/rustling/target/debug/rustling: /home/praise/Documents/GitHub/rust-codes/rustling/src/main.rs
